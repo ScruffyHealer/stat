d499dbb72322806bcf8cbc53dcb890af48a79771
Deni Pesic <pesic.deni@gmail.com>
2015-09-16 11:51:55 +0200

    Revert "Added 'next' to function 'about' in pagescontroller.php"
    This reverts commit 671d425f3a9fb3f32d58319a7723f7a856c53d4d.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,10 +13,8 @@ class PagesController extends Controller
     public function about()
     {
        return view('pages.about')->with([
-           'first' => 'Jeffrey',
-           'last' => 'Way',
-           'next' => 'Madeup'
-
+        'first' => 'Jeffrey',
+        'last' => 'Way'
        ]);
     }
     
